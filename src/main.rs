266b882532ef901c2ac9e3082d38c74597e07121
Wojciech Olejnik <divoolej@gmail.com>
2019-02-02 19:11:41 +0100

    Add input files validation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use structopt::StructOpt;
 use std::path::{Path, PathBuf};
+use std::process;
+use std::fs;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -9,6 +11,37 @@ struct Opt {
   output: Option<PathBuf>
 }
 
+fn error(message: String) {
+  eprintln!("Error: {}", message);
+  process::exit(1);
+}
+
+fn check_input_files(input: PathBuf) -> Vec<String> {
+  if let Ok(metadata) = fs::metadata(&input) {
+    if metadata.is_dir() {
+      let mut sources: Vec<String> = Vec::new();
+      let contents = input.read_dir().unwrap();
+      for entry in contents {
+        let path = entry.unwrap().path();
+        if let Some(ext) = path.extension() {
+          if ext.to_str().unwrap() == "lua" {
+            sources.push(path.file_name().unwrap().to_str().unwrap().to_string());
+          }
+        }
+      }
+      if sources.len() == 0 {
+        error(format!("No *.p8 files found in the input directory ({:?})", input));
+      }
+      return sources;
+    } else {
+      error(format!("{:?} is a file, not a directory.", input));
+    }
+  } else {
+    error(format!("{:?} needs to be a valid directory.", input));
+  }
+  panic!("Failed to validate input directory. Please report this bug.");
+}
 fn main() {
   let opt = Opt::from_args();
+  let sources = check_input_files(opt.input);
 }
