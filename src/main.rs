34c906897e02c26dfa9f7c5f2e734efa6b908d10
Wojciech Olejnik <divoolej@gmail.com>
2019-02-02 19:10:55 +0100

    Declare CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use structopt::StructOpt;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+  #[structopt(short = "i", long = "input", parse(from_os_str), default_value = "src/")]
+  input: PathBuf,
+  #[structopt(parse(from_os_str))]
+  output: Option<PathBuf>
+}
+
 fn main() {
-    println!("Hello, world!");
+  let opt = Opt::from_args();
 }
