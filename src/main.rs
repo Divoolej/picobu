29dff8ae611d3a27f80d1499192adb2a922a891f
Wojciech Olejnik <divoolej@gmail.com>
2019-02-02 19:11:51 +0100

    Add output file validation

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,59 @@ fn check_input_files(input: PathBuf) -> Vec<String> {
   }
   panic!("Failed to validate input directory. Please report this bug.");
 }
+
+fn check_output_file(output: Option<PathBuf>) -> String {
+  if let Some(path) = output {
+    if let Ok(metadata) = fs::metadata(&path) {
+      if metadata.is_file() {
+        if let Some(ext) = path.extension() {
+          if ext == "p8" {
+            return path.file_name().unwrap().to_str().unwrap().to_string();
+          } else {
+            error(format!("{:?} is not a valid *.p8 cartridge.", path));
+          }
+        } else {
+          error("output file needs to have the .p8 extension.".to_string());
+        }
+      } else {
+        error(format!("{:?} is not a valid *.p8 cartridge.", path));
+      }
+    } else {
+      fs::File::create(&path).unwrap();
+      return path.file_name().unwrap().to_str().unwrap().to_string();
+    }
+  } else {
+    println!("Output name not specified, looking for a *.p8 file in the current directory..");
+    let mut outputs: Vec<String> = Vec::new();
+    let contents = fs::read_dir(".").unwrap();
+    for entry in contents {
+      let path = entry.unwrap().path();
+      if let Some(ext) = path.extension() {
+        if ext.to_str().unwrap() == "p8" {
+          outputs.push(path.file_name().unwrap().to_str().unwrap().to_string());
+        }
+      }
+    }
+    if outputs.len() == 0 {
+      let path = Path::new(".").canonicalize().unwrap();
+      let dir = path.as_path().file_name().unwrap().to_str().unwrap().to_string();
+      let dir = format!("{}.p8", dir);
+      println!("No *.p8 files found, generating a new one using the current directory's name ({})..", dir);
+      fs::File::create(&dir).unwrap();
+      return dir;
+    } else if outputs.len() == 1 {
+      println!("Found a *.p8 file - {:?} will be used as the compilation output.", outputs[0]);
+      return outputs.pop().unwrap();
+    } else {
+      error("Found more than one *.p8 file. Please specify the desired output in the arguments.".to_string());
+    }
+  }
+  panic!("Failed to validate the output file. Please report this bug.");
+}
+
 fn main() {
   let opt = Opt::from_args();
   let sources = check_input_files(opt.input);
+  let output = check_output_file(opt.output);
+  println!("Compiling {:?} into {:?}", sources, output);
 }
